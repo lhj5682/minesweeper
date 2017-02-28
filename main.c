ee3aa6335af458abafa58456f0482d96aa9037b3
Outerskyb <outerskyb@gmail.com>
2017-02-28 10:37:59 +0900

    main.c업데이트

---
+++ main.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+#include<conio.h>
+#define WIDTH 20
+#define HEIGHT 20
+int map[WIDTH][HEIGHT];
+typedef struct _op {
+	int x;
+	int y;
+}op;
+
+op position;
+int main(void) {
+
+}
